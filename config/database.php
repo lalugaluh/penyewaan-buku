a2c9a1d082d637fdaf580865d8eb17a7b4dd6a4c
Galuh <lalugaluhhaibara@gmail.com>
2020-08-03 00:48:17 +0800

    set db config untuk heroku dan tambah procfile

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => 'db4free.net',
             'port' => env('DB_PORT', '3306'),
-            'database' => 'buku',
-            'username' => 'root',
-            'password' => '',
+            'database' => 'penyewaan_buku',
+            'username' => 'haibar',
+            'password' => 'acerram4gb',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
